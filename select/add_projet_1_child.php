4242a2c52233976ab4d98ef004b15d638f99c42d
luvumbu <luvumbu.n@gmail.com>
2024-05-02 01:17:27 +0200

    affiche valeur de child ok

--- select/add_projet_1_child.php
+++ select/add_projet_1_child.php
@@ -17,25 +17,54 @@ $databaseHandler = new DatabaseHandler($username, $password);
 
 $databaseHandler->getDataFromTable("SELECT * FROM `liste_projet_admin` WHERE `liste_projet_admin_sha1_parent`=".$liste_projet_admin_sha1_parent." ","liste_projet_admin_id_sha1");
 
-echo $liste_projet_admin_sha1_parent ;
 
- var_dump($databaseHandler->tableList_info);
+$databaseHandler_1 = new DatabaseHandler($username, $password);
+
+$databaseHandler_1->getDataFromTable("SELECT * FROM `liste_projet_admin` WHERE `liste_projet_admin_sha1_parent`=".$liste_projet_admin_sha1_parent." ","liste_projet_admin_name1");
 
 
+$databaseHandler_2 = new DatabaseHandler($username, $password);
+
+$databaseHandler_2->getDataFromTable("SELECT * FROM `liste_projet_admin` WHERE `liste_projet_admin_sha1_parent`=".$liste_projet_admin_sha1_parent." ","liste_projet_admin_name2");
+
+
+$databaseHandler_3 = new DatabaseHandler($username, $password);
+
+$databaseHandler_3->getDataFromTable("SELECT * FROM `liste_projet_admin` WHERE `liste_projet_admin_sha1_parent`=".$liste_projet_admin_sha1_parent." ","liste_projet_admin_name3");
+
+
+$databaseHandler_4 = new DatabaseHandler($username, $password);
+
+$databaseHandler_4->getDataFromTable("SELECT * FROM `liste_projet_admin` WHERE `liste_projet_admin_sha1_parent`=".$liste_projet_admin_sha1_parent." ","liste_projet_admin_name4");
+
+
+$databaseHandler_5 = new DatabaseHandler($username, $password);
+
+$databaseHandler_5->getDataFromTable("SELECT * FROM `liste_projet_admin` WHERE `liste_projet_admin_sha1_parent`=".$liste_projet_admin_sha1_parent." ","liste_projet_admin_name5");
+
+
+
+ 
+
+ 
+
+
+
+ 
 
 
  for($a = 0 ; $a <count($databaseHandler->tableList_info) ; $a ++) {
       ?>
 
 <div class="header">
-  <h2> <input type="text"  title="<?php echo $databaseHandler->tableList_info[$a] ?>"  onkeyup="add_projet_1_key_up2(this)" id="<?php echo $databaseHandler->tableList_info[$a]."_1" ?>"></h2>
+  <h2> <input type="text"  value="<?php echo $databaseHandler_1->tableList_info[$a] ?>" title="<?php echo $databaseHandler->tableList_info[$a] ?>"  onkeyup="add_projet_1_key_up2(this)" id="<?php echo $databaseHandler->tableList_info[$a]."_1" ?>"></h2>
 </div>
 
-<h2><input type="text"  title="<?php echo $databaseHandler->tableList_info[$a] ?>"  onkeyup="add_projet_1_key_up2(this)" id="<?php echo $databaseHandler->tableList_info[$a]."_2" ?>"></h2>
-      <h5><input type="text"  title="<?php echo $databaseHandler->tableList_info[$a] ?>"  onkeyup="add_projet_1_key_up2(this)" id="<?php echo $databaseHandler->tableList_info[$a]."_3" ?>"></h5>
+<h2><input type="text"  value="<?php echo $databaseHandler_2->tableList_info[$a] ?>" title="<?php echo $databaseHandler->tableList_info[$a] ?>"  onkeyup="add_projet_1_key_up2(this)" id="<?php echo $databaseHandler->tableList_info[$a]."_2" ?>"></h2>
+      <h5><input type="text"  value="<?php echo $databaseHandler_3->tableList_info[$a] ?>" title="<?php echo $databaseHandler->tableList_info[$a] ?>"  onkeyup="add_projet_1_key_up2(this)" id="<?php echo $databaseHandler->tableList_info[$a]."_3" ?>"></h5>
       <div class="fakeimg" style="height:200px;">Image</div>
-      <p><input type="text"  title="<?php echo $databaseHandler->tableList_info[$a] ?>"  onkeyup="add_projet_1_key_up2(this)" id="<?php echo $databaseHandler->tableList_info[$a]."_4" ?>"></p>
-      <p><input type="text"  title="<?php echo $databaseHandler->tableList_info[$a] ?>"  onkeyup="add_projet_1_key_up2(this)" id="<?php echo $databaseHandler->tableList_info[$a]."_5" ?>">
+      <p><input type="text"  value="<?php echo $databaseHandler_4->tableList_info[$a] ?>" title="<?php echo $databaseHandler->tableList_info[$a] ?>"  onkeyup="add_projet_1_key_up2(this)" id="<?php echo $databaseHandler->tableList_info[$a]."_4" ?>"></p>
+      <p><input type="text"  value="<?php echo $databaseHandler_5->tableList_info[$a] ?>" title="<?php echo $databaseHandler->tableList_info[$a] ?>"  onkeyup="add_projet_1_key_up2(this)" id="<?php echo $databaseHandler->tableList_info[$a]."_5" ?>">
 
       <?php
  }
