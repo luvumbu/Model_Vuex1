8bd34b9b12bb750b6df5cf28c7895cd25a1f4f8b
luvumbu <luvumbu.n@gmail.com>
2024-05-07 00:48:30 +0200

    ajout de convetion du codage ok

--- index.php
+++ index.php
@@ -10,11 +10,14 @@
 </head>
 <body>  
 <?php
-$path_DatabaseHandler ="class/DatabaseHandler.php" ;  
-$form_log_dbd        ="view/off/form_log_dbd.php" ;
-$form_log_dbd_user_on="view/on/form_log_dbd_user_on.php";
-$fichier_connexion = "class/config.php" ;
 
+ 
+$path_DatabaseHandler ='class/DatabaseHandler.php' ;  
+$form_log_dbd         ='view/off/form_log_dbd.php' ;
+$form_log_dbd_user_on ='view/on/form_log_dbd_user_on.php';
+$fichier_connexion    ='class/config.php' ;
+$reload_page          ='<meta http-equiv="refresh" content="0;URL=">';
+ 
 require_once $path_DatabaseHandler; 
 
 if (file_exists($fichier_connexion)) {
@@ -22,14 +25,14 @@ require_once $fichier_connexion;
 $databaseHandler = new DatabaseHandler($username, $password); 
 if(!$databaseHandler->verif) {   
     unlink($fichier_connexion) ;     
-   echo '<meta http-equiv="refresh" content="0;URL=">' ;
+    echo   $reload_page  ;
 } 
 
 if( $databaseHandler->existance_table("information_user")==0){
   // verification de l'existance de la table information_user
   // si la table n'existe pas on efface le fichier configuration et on recharge la page
   unlink($fichier_connexion) ;     
-  echo '<meta http-equiv="refresh" content="0;URL=">' ;
+  echo   $reload_page  ;
 }
  require_once $form_log_dbd_user_on;   
 } 
